4d970731bc41fb9974fe65ba724f7f1abd9b853c
george murphy <georgeolivermurphy@hotmail.com>
2022-02-09 12:43:45 +0000

    toggle for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
@@ -7,8 +8,9 @@ const NavBarItem = ({ title, classprops }) => (
   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 );
 
-
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
     <nav className="w-full flex md:justify center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -22,6 +24,14 @@ const Navbar = () => {
           Login
         </li>
       </ul>
+       <div className="flex relative">
+        {!toggleMenu && (
+          <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+        )}
+        </div>
     </nav>
     );
 }
