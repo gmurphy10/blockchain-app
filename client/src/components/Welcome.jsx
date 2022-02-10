01a0cf1af1f4842b5a9a0e3e11f9f2e5f18be9fe
george murphy <georgeolivermurphy@hotmail.com>
2022-02-10 10:29:01 +0000

    Addiitonal Features

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -37,8 +37,22 @@ const Welcome = () => {
 
                   <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                     <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
-                    Reliability
+                      Reliability
                     </div>
+                    <div className={companyCommonStyles}>Security</div>
+                    <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+                      Ethereum
+                    </div>
+                    <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+                      Web 3.0
+                    </div>
+                    <div className={companyCommonStyles}>Low Fees</div>
+                    <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+                      Blockchain
+                    </div>
+                  </div>
+                </div>
+
 
 
 
