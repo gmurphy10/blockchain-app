a54d673edd95bc9dca2982386266733a97543749
george murphy <georgeolivermurphy@hotmail.com>
2022-02-10 10:22:57 +0000

    connect wallet funct & styling of button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,10 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from ".";
 
 const Welcome = () => {
+
+  const connectWallet = () => {
+
+}
   return (
       <div className="flex w-full justify-center items-center">
          <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -16,6 +20,21 @@ const Welcome = () => {
                  <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                   Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                 </p>
+
+                 <button
+                    type="button"
+                    onClick={connectWallet}
+                    className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                  >
+                    <AiFillPlayCircle className="text-white mr-2" />
+                    <p className="text-white text-base font-semibold">
+                      Connect Wallet
+                    </p>
+                  </button>
+                  )}
+
+
+
              </div>
         </div>
       </div>
